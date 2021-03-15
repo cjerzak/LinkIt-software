c437027862cf77f7941c423fcdce76c270c5197e
cjerzak <connor.jerzak@gmail.com>
2021-03-15 11:34:21 -0400

    tmp

--- tutorial.R
+++ tutorial.R
@@ -11,7 +11,7 @@ x_mat <- data.frame("xname"=c("apple computers","j p morgan"),
 y_mat <- data.frame("yname"=c("apple inc","jp morgan"),
                     "ydat"=c(rnorm(2)))
 
-z_red_LinkIt_markov <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
+z_LinkIt_markov <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
                           by.x = "xname",by.y="yname",
                           openBrowser=F,
                           algorithm = "markov", returnDiagnostics = T,
@@ -23,7 +23,7 @@ z_red_LinkIt_markov <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
                                          matchMethod = "jw",
                                          qgram = 2))
 
-z_red_LinkIt_bipartite <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
+z_LinkIt_bipartite <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
                               by.x = "xname",by.y="yname",
                               fuzzy_step = T, openBrowser=F,
                               algorithm = "bipartite", returnDiagnostics = T,
@@ -40,7 +40,7 @@ z_red_LinkIt_bipartite <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
 #pip install keras
 #pip install chars2vec
 #make sure pip is using same python version as recitulate 
-z_red_LinkIt_ml <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
+z_LinkIt_ml <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
                        by.x = "xname",by.y="yname",
                        fuzzy_step = T, openBrowser=F,
                        algorithm = "ml", returnDiagnostics = T,
@@ -52,8 +52,12 @@ z_red_LinkIt_ml <- LinkIt(x=as.data.table(x_mat), y=as.data.table(y_mat),
                                        matchMethod = "jw",
                                        qgram = 2))
 
-z_red_fuzzy_FULL <- FastFuzzyMatch(x_red,
-                                   y_red,
+z_fuzzy     <-      FastFuzzyMatch(x_mat,y_mat,
                                    by.x="xname", by.y= "yname",
                                    method = "jw", max_dist = 0.1,
                                    q = 2,browser=F)  
+
+
+
+
+
